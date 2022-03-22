77070bd81523d258b58879b11b84fe461cc809de
jasim11 <51187260+jasim11@users.noreply.github.com>
2022-03-22 16:29:36 +0300

    Update index.js

--- server/index.js
+++ server/index.js
@@ -20,7 +20,7 @@ app.use('/posts', postRoutes);
 app.use("/user", userRouter);
 
 const CONNECTION_URL = 'mongodb+srv://master123:123@cluster0.vttub.mongodb.net/myFirstDatabase?retryWrites=true&w=majority';
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT ;
 
 mongoose.connect(process.env.CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
     .then(() => app.listen(PORT, () => console.log(`Server Running on Port: http://localhost:${PORT}`)))
